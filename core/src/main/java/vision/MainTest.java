eb32112facd93b1ce282584a8b1fe0095c660fc1
qq5132834 <513283439@qq.com>
2024-04-09 17:08:16 +0800

    交互式 repl，脚本运行

---
+++ core/src/main/java/vision/MainTest.java
@@ -0,0 +1,9 @@
+package vision;
+
+import replpp.Main;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
--- core/src/main/scala/replpp/Main.scala
+++ core/src/main/scala/replpp/Main.scala
@@ -7,8 +7,10 @@ object Main {
     val config = Config.parse(args)
 
     if (config.scriptFile.isDefined) {
+      //脚本运行
       ScriptRunner.main(args)
     } else {
+      //交互式shell运行
       InteractiveShell.run(config)
     }
   }
