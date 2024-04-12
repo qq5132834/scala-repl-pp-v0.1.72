b0e439cdcb42e422401a57b9b9962a8e31f13939
qq5132834 <513283439@qq.com>
2024-04-12 15:20:05 +0800

    执行脚本

--- core/src/main/scala/replpp/Main.scala
+++ core/src/main/scala/replpp/Main.scala
@@ -4,9 +4,11 @@ import replpp.scripting.ScriptRunner
 
 object Main {
   def main(args1: Array[String]): Unit = {
+    println("aaabbbccc")
     val args = Array(
-                      "--greeting", "welcome to use vision.", // 问候
-                      "--prompt", "vision" // Terminal 提示语
+                      "--greeting", "welcome to use vision.",             // 问候
+                      "--prompt", "vision",                               // Terminal 提示语
+                      "--script", "core/src/main/resources/scripts/script.scala"     //执行脚本
                      )
     val config = Config.parse(args)
 
